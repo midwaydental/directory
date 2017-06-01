a60be753bc624c5174c36a8b10645798cc4288e7
midwaydental <cmehta@midwaydental.com>
2017-06-01 16:39:59 -0400

    App deployed
    Initial all file uploads

---
+++ controller.js
@@ -0,0 +1,35 @@
+/**
+ * Created by CS-5 on 4/5/2017.
+ */
+var app = angular
+    .module('midwaydental', []);
+
+
+app.controller('RecipeController', ['$http','$scope','$location','$timeout', function ($http, $scope, $location, $timeout) {
+    $scope.list1='chetas';
+
+$scope.fname = '';
+    $scope.list = null;
+
+
+    $http.get('directory.JSON').success(function (response) {
+       $scope.list = response;
+
+       console.log($scope.list);
+
+    });
+
+
+    $scope.propertyName = '';
+    $scope.reverse = true;
+    $scope.sortBy = function(propertyName) {
+        $scope.reverse = ($scope.propertyName === propertyName) ? !$scope.reverse : false;
+        $scope.propertyName = propertyName;
+    };
+
+
+
+}]);
+
+
+
